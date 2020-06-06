d116497cc20a92811f592eeb98a5febca759d7ca
mjrosenberg <mjr70@duke.edu>
2020-06-06 14:38:53 -0700

    fixed auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,7 +5,7 @@ const utils = require('../lib/hashUtils');
 const db = require('../db');
 
 module.exports.createSession = (req, res, next) => {
-  console.log('req', req);
+  //console.log('req', req);
   Promise.resolve(req.cookies.shortlyid)
     .then((hash) => {
       if (!hash) {
@@ -14,39 +14,24 @@ module.exports.createSession = (req, res, next) => {
         return models.Sessions.get({hash: hash});
       }
     })
-    .then((session) =>{
+    .tap((session) =>{
       if (session === undefined ) {
         throw session;
       }
     })
-    .catch((hash) => { return models.Sessions.create(); })
+    .catch(() => {
+      return models.Sessions.create()
+        .then((session) => {
+          return models.Sessions.get({ id: session.insertId });
+        })
+        .tap((session) => {
+          res.cookie('shortlyid', session.hash);
+        });
+    })
     .then((session) => {
-      console.log('session returned from created', session);
-      req.session = {};
-      //req.session = session;
-      //req.cookie.shortlyid = req.session.hash;
-      //}
-      //.then()
-      let data1 = utils.createRandom32String();
-      req.session.hash = utils.createHash(data1);//maybe need to pull the hash from the row that was just created
-      req.session.user = {};
-      req.session.userId = 1;//session.insertId;
-      req.session.user.username = 'BillZito'; //need to not manually assign, manually assigning those both makes the tests pass
-      cookieParser(req, res, ()=>{
-        //console.log('req.cookies', req.cookies);
-        //res.cookies = req.cookies;
-        if (res.cookies.shortlyid === undefined) {
-          res.cookies.shortlyid = {value: req.session.hash};
-        } else {
-          res.cookies.shortlyid.value = req.cookies.shortlyid;
-        }
-      });
+      req.session = session;
       next();
     });
-
-  // req.session = {};
-  // req.session.hash =
-  // res.json(cookieParser(req));
 };
 
 
